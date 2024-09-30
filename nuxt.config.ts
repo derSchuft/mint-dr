a2975e5f9bbdd111083e598b6ede7bbbc08b6b8c
Daniel Rotter <dani@differencemedia.de>
2024-09-30 17:23:30 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,45 +1,17 @@
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
-
-const currentDir = dirname(fileURLToPath(import.meta.url))
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-  runtimeConfig: {
-    public: {
-
-      projects: [
-        {
-          title: 'PunkApepen',
-          image: 'https://i.seadn.io/s/primary-drops/0xf39be779905d16fe23b2cc1297dc3e759d2daa11/32043792:about:media:9d46a3e3-7c51-4937-9580-c5497021f280.png?auto=format&dpr=1&w=750',
-          link: 'https://www.punkapepen.com/'
-        },
-        {
-          title: 'itmightbeart',
-          image: 'https://i.seadn.io/s/primary-drops/0x6d01521974c91f76e7d8d62f30c6e1f74b0b7b1c/33360848:about:media:23467104-316f-4eca-bedf-14151104cd46.png?auto=format&dpr=1&w=750',
-          link: 'https://app.itmightbe.art/',
-        },
-        
-      ]
-    }
-  },
   
   alias: {
     '@base': '@visualizevalue/mint-app-base',
   },
 
-  extends: [
-    //'..', // Extend our theme
-    '@visualizevalue/mint-app-base', // Extend the base layer
-  ],
-
   css: [
     '@base/assets/styles/index.css', // Extend the base theme styles
-    join(currentDir, './assets/styles/theme.css'), // Add our own styles
+    '~/assets/styles/theme.css', // Add our own styles
   ],
 
+  extends: '@visualizevalue/mint-app-base',
   
-  
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
